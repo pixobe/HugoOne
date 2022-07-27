88b5ecb2cc07708d303333db530aa5020b1c9077
Pixobe <pixobe.com@gmail.com>
2022-07-27 14:24:39 +0800

    Before I loose it again

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,45 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["content/**/*.md", "layouts/**/*.html"],
+  mode: 'jit',
+  content: [
+    "content/**/*.md", "layouts/**/*.html"
+  ],
+  darkMode: "media",
   theme: {
-    colors: {
-      light: {
-        DEFAULT: "#E7E3E6",
-        100: '#DBD9DA',
-      },
-      dark: {
-        DEFAULT: "#414042",
-        100: "#1b191a",
-        200: "#1a1819"
-      },
-      primary: {
-        DEFAULT: "#d9593e",
-        300: "#e48b78",
-        800: "#c35038"
-      },
-      white: "#ffffff",
-      gray: {
-        900: "#111827",
-        800: "#1f2937",
-        700: "#374151",
-        500: "#6b7280",
-        400: "#9ca3af",
-        300: "#d1d5db",
-        200: "#e5e7eb",
-        100: "#f3f4f6"
-      },
-      slate: {
-        100: "#f1f5f9",
-        200: "#e2e8f0",
-        300: "#cbd5e1"
-      }
-    },
-    fontFamily: {
-      "logo": ['Figtree'],
-      "body": ['Figtree'],
-    },
-    extend: {
-    },
+    extend: {},
   },
-  plugins: [],
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require("daisyui"),
+    require('@tailwindcss/typography'),
+  ],
+  daisyui: {
+    themes: [
+      {
+        hugotech: {
+          primary: "#5D35CA",
+          secondary: "#F000B8",
+          accent: "#37CDBE",
+          neutral: "#3D4451",
+          "base-100": "#FFFFFF",
+          info: "#3ABFF8",
+          success: "#22BAA1",
+          warning: "#FBBD23",
+          error: "#F87272",
+        },
+      },
+    ],
+  },
 }
