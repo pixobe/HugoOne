ac86c13f04541666e49bd86e646a60126966ba73
Pixobe <pixobe.com@gmail.com>
2022-09-16 21:08:31 +0800

    Before changing header

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,7 @@ module.exports = {
     plugins: [
         require("daisyui"),
         require('@tailwindcss/typography'),
+        require('tailwindcss/nesting')
     ],
     daisyui: {
         themes: [
