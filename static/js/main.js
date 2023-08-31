0d9985c95f650904639d00edebed32d5aef736fa
pixobe <pixobe.com@gmail.com>
2023-08-31 18:33:21 +0800

    form submissions

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,4 @@
 
-window.onload = function () {
-    //var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
-    breadcrumbInit();
-}
 
 function breadcrumbInit() {
     const mobileMenu = document.getElementById("mobile-menu");
@@ -23,4 +19,74 @@ function breadcrumbInit() {
         })
     }
 
+}
+
+/**
+ * Submit forms
+ */
+async function submitForm(name, data) {
+
+    try {
+        // URL to which the POST request is sent
+        const url = `http://localhost:3000/forms`;
+        // Options for the fetch request
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'x-form-name': name,
+            },
+            body: JSON.stringify(data)
+        };
+        // Make the POST request
+        return fetch(url, options)
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return;
+            })
+            .catch((error) => {
+                console.error('Error in POST request:', error);
+            });
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+window.onload = function () {
+    //var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+    breadcrumbInit();
+
+    // register form submissions
+    const submissionForms = document.querySelectorAll("[data-pixobe]");
+
+    if (submissionForms) {
+        submissionForms.forEach(formElement => {
+
+            formElement.addEventListener("submit", async (e) => {
+                e.preventDefault();
+                const target = e.target;
+                const dataset = target.dataset;
+                const formData = new FormData(formElement);
+                const data = {};
+
+                formData.forEach((value, key) => {
+                    data[key] = value;
+                });
+
+                const msg = formElement.querySelector("#msg");
+                
+                msg.innerText ="Submitting please wait...";
+                formElement.append(msg);
+
+                return submitForm(dataset.pixobe, { data })
+                    .then(() => { })
+                    .finally(() => {
+                        msg.innerText="Thank you for subscribing!";
+                    })
+
+            })
+        })
+    }
 }
